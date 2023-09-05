e5ac60ab94f2fadc771835d4143a3beb7dc207f6
lingkerio <lingkerio@qq.com>
2023-09-06 02:04:42 +0800

    solva problem optional3

--- proj/ants/ants.py
+++ proj/ants/ants.py
@@ -546,7 +546,7 @@ class TankAnt(ContainerAnt):
     food_cost = 6
     # OVERRIDE CLASS ATTRIBUTES HERE
     # BEGIN Problem Optional 3
-    implemented = False   # Change to True to view in the GUI
+    implemented = True   # Change to True to view in the GUI
     # END Problem Optional 3
 
     def __init__(self, armor=2):
@@ -554,7 +554,9 @@ class TankAnt(ContainerAnt):
 
     def action(self, gamestate):
         # BEGIN Problem Optional 3
-        "*** YOUR CODE HERE ***"
+        for bee in self.place.bees.copy():
+            Insect.reduce_armor(bee, self.damage)
+        super().action(gamestate)
         # END Problem Optional 3
 ############
 # Statuses #
